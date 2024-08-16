8f0726fd9c93cea606f240267fb3506cd1a4f9a2
Shashi-Anand <shashianand2600@gmail.com>
2024-08-16 20:59:57 +0530

    setting logo and bg image to the login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { LOGO } from '../utils/constants.js';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-full absolute z-30'>
+      <div className='w-10/12 mx-auto'>
+        <img src={LOGO} alt='logo' className='w-48'/>
+      </div>
+
+    </div>
   )
 }
 
