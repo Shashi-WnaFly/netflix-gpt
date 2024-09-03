cf716f73dcdc5d145f48d40d778256895e4614fd
Shashi-Anand <shashianand2600@gmail.com>
2024-09-03 21:15:32 +0530

    adding now playing movies api data to the store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
-  const movies = async function () {
+  const dispatch = useDispatch();
+  const getNowPlayingMovies = async function () {
     const now_playing = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     )
     const data = await now_playing.json();
-    console.log(data);
+    console.log(data?.results);
+    dispatch(addNowPlayingMovies(data?.results));
   };
 
   useEffect(() => {
-    movies();
+    getNowPlayingMovies();
   }, []);
 
   return <Header />;
